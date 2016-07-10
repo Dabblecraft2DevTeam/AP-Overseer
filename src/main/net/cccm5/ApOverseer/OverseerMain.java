b1574056cac1d9563e71541cfd5e23254ad3eb5c
cccm5 <ccccm5@gmail.com>
2016-07-10 18:04:53 -0400

    Make command toggle

--- src/main/net/cccm5/ApOverseer/OverseerMain.java
+++ src/main/net/cccm5/ApOverseer/OverseerMain.java
@@ -11,79 +11,88 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OverseerMain extends JavaPlugin {
-	private FileConfiguration config = getConfig();
-	private ArrayList<String> chatCommands,excludedCommands;
-	private ArrayList<CommandSender> playerSpy,consoleSpy;
-	private boolean includeChat;
-	@Override
+    private FileConfiguration config = getConfig();
+    private ArrayList<String> chatCommands,excludedCommands;
+    private ArrayList<CommandSender> playerSpy,consoleSpy;
+    private boolean includeChat;
+    @Override
     public void onEnable() {
-		//////Configs//////
-		config.addDefault("Include chat messages",false);
-		//default list of messaging commands
-		ArrayList<String> tempCommands = new ArrayList<String>();
-		tempCommands.add("/r");
-		tempCommands.add("/reply");
-		tempCommands.add("/mail");
-		tempCommands.add("/message");
-		tempCommands.add("/m");
-		tempCommands.add("/whisper");
-		tempCommands.add("/ch qm");
-		tempCommands.add("/helpop");
-		config.addDefault("Chat message commands", tempCommands);
-		//default list of excluded commands
-		tempCommands=new ArrayList<String>();
-		tempCommands.add("/help");
-		tempCommands.add("/rules");
-		config.addDefault("Excluded Commands", tempCommands);
-		config.options().copyDefaults(true);
+        //////Configs//////
+        config.addDefault("Include chat messages",false);
+        //default list of messaging commands
+        ArrayList<String> tempCommands = new ArrayList<String>();
+        tempCommands.add("/r");
+        tempCommands.add("/reply");
+        tempCommands.add("/mail");
+        tempCommands.add("/message");
+        tempCommands.add("/m");
+        tempCommands.add("/whisper");
+        tempCommands.add("/ch qm");
+        tempCommands.add("/helpop");
+        config.addDefault("Chat message commands", tempCommands);
+        //default list of excluded commands
+        tempCommands=new ArrayList<String>();
+        tempCommands.add("/help");
+        tempCommands.add("/rules");
+        config.addDefault("Excluded Commands", tempCommands);
+        config.options().copyDefaults(true);
         this.saveConfig();
         saveDefaultConfig();
         //////Load Config//////
         includeChat = getConfig().getBoolean("Include chat messages");
         chatCommands.addAll(getConfig().getStringList("Chat message commands"));
         excludedCommands.addAll(getConfig().getStringList("Excluded Commands"));
-        
+
         //getServer().getPluginManager().registerEvents(this, this);
-	}
-	@Override
+    }
+
+    @Override
     public void onDisable() {
     }
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("playerSpy")){
-			//if(sender.hasPermission("ApOverseer.playerSpy")){
-				playerSpy.add(sender);
-				return true;
-			}
-			//else
-			//{
-				//sender.sendMessage("You don't have permision for that!");
-				//return false;
-			//}
-		//}
-		return false;
-	}
-	
-	@EventHandler
-	public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent event){
-		boolean testState=true;
-		if(!includeChat)
-		{
-			for(String testString : chatCommands)
-			{
-				if(event.getMessage().toLowerCase().startsWith(testString))
-					testState=false;
-			}
-		}
-		for(String testString : excludedCommands)
-		{
-			if(event.getMessage().toLowerCase().startsWith(testString))
-				testState=false;
-		}
-		if(testState)
-			for(CommandSender observer: playerSpy){	
-				observer.sendMessage(event.getPlayer().getDisplayName() + ": " + event.getMessage());	
-		}
-	}
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("playerSpy")){
+            if(sender.hasPermission("ApOverseer.playerSpy")){
+                if(!playerSpy.contains(sender))
+                {
+                    playerSpy.add(sender);
+                    sender.sendMessage("Started observing player commands");
+                    return true;
+                }else{
+                    playerSpy.remove(sender);
+                    sender.sendMessage("stopped observing player commands");
+                    return true;
+                }
+            }
+            else
+            {
+                sender.sendMessage("You don't have permision for that!");
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void onPlayerCommandPreprocessEvent(PlayerCommandPreprocessEvent event){
+        boolean testState=true;
+        if(!includeChat)
+        {
+            for(String testString : chatCommands)
+            {
+                if(event.getMessage().toLowerCase().startsWith(testString))
+                    testState=false;
+            }
+        }
+        for(String testString : excludedCommands)
+        {
+            if(event.getMessage().toLowerCase().startsWith(testString))
+                testState=false;
+        }
+        if(testState)
+            for(CommandSender observer: playerSpy){	
+                observer.sendMessage(event.getPlayer().getDisplayName() + ": " + event.getMessage());	
+            }
+    }
 }
 
