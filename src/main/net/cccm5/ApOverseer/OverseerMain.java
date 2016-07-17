b907039525cce7294c7b547711dea826acdfa06c
cccm5 <ccccm5@gmail.com>
2016-07-16 20:08:33 -0400

    disable console listener and add custom colors

--- src/main/net/cccm5/ApOverseer/OverseerMain.java
+++ src/main/net/cccm5/ApOverseer/OverseerMain.java
@@ -2,6 +2,8 @@ package net.cccm5.ApOverseer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -27,6 +29,7 @@ public final class OverseerMain extends JavaPlugin implements Listener {
     private FileConfiguration config = getConfig();
     private List<String> chatCommands,excludedCommands;
     private List<CommandSender> commandSpy=new ArrayList<CommandSender>();
+    private HashMap<String, String> nameColor = new HashMap<String, String>();
     private boolean includeChat;
     @Override
     public void onEnable() {
@@ -57,8 +60,11 @@ public final class OverseerMain extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
         //getServer().getPluginManager().registerEvents(this, this);
         //set up logger
-        Logger log = (Logger) LogManager.getRootLogger();
-        log.addAppender(new Log4JAppender());
+
+        //disabled for now
+
+        //Logger log = (Logger) LogManager.getRootLogger();
+        //log.addAppender(new Log4JAppender());
     }
 
     @Override
@@ -66,22 +72,56 @@ public final class OverseerMain extends JavaPlugin implements Listener {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(cmd.getName().equalsIgnoreCase("commandSpy")){
-            if(sender.hasPermission("ApOverseer.commandSpy")){
-                if(!commandSpy.contains(sender))
+        if(cmd.getName().equalsIgnoreCase("overseer")){
+            if(!sender.hasPermission("ApOverseer.main"))
+            {
+                sender.sendMessage("§4You don't have permission to do that");
+                return true;
+            }
+            if(args.length==0 || args[0].equalsIgnoreCase("help"))
+            {
+                sender.sendMessage("§a-----§bAP Overseer§a-----");
+                sender.sendMessage("§a/overseer help:    §bview this message");
+                sender.sendMessage("§a/overseer commandSpy: §bsend command inputs to you");
+                sender.sendMessage("§a/overseer color <color>:   §bset the output color of commands"); 
+            }
+            if(args[0].equalsIgnoreCase("commandSpy")){
+                if(sender.hasPermission("ApOverseer.commandSpy")){
+                    if(!commandSpy.contains(sender))
+                    {
+                        commandSpy.add(sender);
+                        sender.sendMessage("Started observing player commands");
+                        return true;
+                    }else{
+                        commandSpy.remove(sender);
+                        sender.sendMessage("Stopped observing player commands");
+                        return true;
+                    }
+                }
+                else
                 {
-                    commandSpy.add(sender);
-                    sender.sendMessage("Started observing player commands");
-                    return true;
-                }else{
-                    commandSpy.remove(sender);
-                    sender.sendMessage("stopped observing player commands");
+                    sender.sendMessage("§4You don't have permision for that!");
                     return true;
                 }
             }
-            else
+        }
+        if(cmd.getName().equalsIgnoreCase("overseerColor")){
+            if(args.length!=2)
             {
-                sender.sendMessage("You don't have permision for that!");
+                sender.sendMessage("§4Error: correct format is /overseer color <color>");
+                return true;
+            }
+            if(args[1].length()!=1 || !(args[1].toLowerCase().charAt(0)>='a' && args[1].toLowerCase().charAt(0)<='f') || !(args[1].charAt(0)>='0' && args[1].charAt(0)<='9'))
+            {
+                sender.sendMessage("§4Error: \"" + args[1] + "\" is not a color code");
+                return true;
+            }
+            if(sender.hasPermission("ApOverseer.setColor")){
+                nameColor.put(sender.getName(),args[1]);
+                sender.sendMessage("§acolor set to §" + args[1] + "&" +args[1]);
+                return true;
+            }else{
+                sender.sendMessage("§4You don't have permission for that!");
                 return true;
             }
         }
@@ -107,7 +147,7 @@ public final class OverseerMain extends JavaPlugin implements Listener {
         if(testState)
             for(CommandSender observer: commandSpy){	
                 if(!(observer instanceof Player) && !(observer==event.getPlayer()))
-                    observer.sendMessage(event.getPlayer().getDisplayName() + ": " + event.getMessage());	
+                    observer.sendMessage(event.getPlayer().getDisplayName() + ": §" + nameColor.get(event.getPlayer().getName())+ event.getMessage());	
             }
     }
 
@@ -131,7 +171,7 @@ public final class OverseerMain extends JavaPlugin implements Listener {
         if(testState)
             for(CommandSender observer: commandSpy){
                 if((observer instanceof Player))
-                    observer.sendMessage("console: " + event.getCommand());	
+                    observer.sendMessage("console: §" + nameColor.get(event.getSender().getName()) + event.getCommand());	
             }
     }
 
